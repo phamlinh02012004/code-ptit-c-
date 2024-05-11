b62fef1f67bba499a1db056a881ec7a942b1f11e
phamlinh02012004 <phamhailinh1345@gmail.com>
2024-05-12 01:54:05 +0700

    12/5/2023

--- số lộc phát.cpp
+++ số lộc phát.cpp
@@ -1,24 +1,18 @@
-#include<bits/stdc++.h>
-#include <cmath>
-#include <cstdio>
-#include <vector>
 #include <iostream>
-#include <algorithm>
 using namespace std;
-int main(){
-	int t;
-	cin >> t;
-	while(t--){
-		 string s;
-		
-		cin >> s;
-		int check  = 0;
-		for(int i = 0 ; i < s.size(); i++){
-			if(s[i] != '0' && s[i] != '6' && s[i] != '8'){
-				check = 1;
-			}
-		}
-		if(check == 0) cout << "YES" << endl;
-		else cout << "NO" << endl;
-	}
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        char c;
+        cin >> c;
+
+        if (isupper(c)) {
+            cout << char(tolower(c)) << endl;
+        } else if (islower(c)) {
+            cout << char(toupper(c)) << endl;
+        }
+    }
+    return 0;
 }
